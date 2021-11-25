537e001921902e9dcb6f8f1932b240eb4ab338dc
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2021-11-25 14:19:12 -0500

    Bump version from 0.3.6 to 0.3.7

--- package_locator/__main__.py
+++ package_locator/__main__.py
@@ -5,7 +5,7 @@ from rich import traceback
 
 
 @click.command()
-@click.version_option(version="0.3.6", message=click.style("package-locator Version: 0.3.6"))
+@click.version_option(version="0.3.7", message=click.style("package-locator Version: 0.3.7"))
 def main() -> None:
     """package-locator."""
 
